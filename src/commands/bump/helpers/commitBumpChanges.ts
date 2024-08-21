1241473feb98b5e14032ba1b96408f94a8d7d460
Tal Kohavy <talkohavy@gmail.com>
2024-08-21 10:41:27 +0300

    added the -u flag to the git add command of bump

--- src/commands/bump/helpers/commitBumpChanges.ts
+++ src/commands/bump/helpers/commitBumpChanges.ts
@@ -11,7 +11,7 @@ async function commitBumpChanges(props: CommitBumpChangesProps) {
 
   mdVersionFilePaths.forEach((mdVersionFileAbsolutePath) => {
     try {
-      execSync(`git add ${mdVersionFileAbsolutePath}`);
+      execSync(`git add -u ${mdVersionFileAbsolutePath}`); // <--- the -u flag states that only delete or modified files are committed. This helps avoid committing new (untracked) files.
     } catch (_error) {
       logger.warn(
         `WARNING! 'git add' operation failed. Detected an md version file which probably was not committed. Path to file was: ${COLORS.yellow}${mdVersionFileAbsolutePath}`,
