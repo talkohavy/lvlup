d5195de7e0590a924923d850117e7b0ea49510e7
Tal Kohavy <talkohavy@gmail.com>
2024-09-01 20:50:26 +0300

    getAllMdVersionFiles should ignore the README.md file

--- src/utils/getMdVersionFilesByPackageName.ts
+++ src/utils/getMdVersionFilesByPackageName.ts
@@ -3,7 +3,9 @@ import { glob } from 'glob';
 import { LVLUP_DIR_NAME, LVLUP_DIR_PATH } from '../constants/globals.js';
 
 async function getAllMdVersionFiles() {
-  const mdVersionFilePathsRaw = await glob(`${LVLUP_DIR_NAME}/*.md`);
+  const mdVersionFilePathsRaw = await glob(`${LVLUP_DIR_NAME}/*.md`, {
+    ignore: [`${LVLUP_DIR_NAME}/README.md`],
+  });
 
   const mdVersionFilePaths = mdVersionFilePathsRaw.map((file) => {
     const filenameWithExtension = file.replace(path.join(LVLUP_DIR_NAME, path.sep), '');
