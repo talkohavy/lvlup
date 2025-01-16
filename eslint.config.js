6ef9835be3bdae92613c27379867f5b86c82da4b
Tal Kohavy <talkohavy@gmail.com>
2025-01-16 06:12:06 +0200

    eslint 9: changed globals to node instead of browser

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ export default [
     // when an `ignores` key is used without any other keys in the configuration object, then it acts as global `ignores`.
     ignores: ['dist'],
   },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
